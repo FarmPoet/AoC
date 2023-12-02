3ad95b32108874bbf026d48cc4ba3d7d7568f9b9
FarmPoet <49650908+FarmPoet@users.noreply.github.com>
2023-12-02 19:15:15 +0000

    Update day2.py

--- 2023/day2.py
+++ 2023/day2.py
@@ -1,10 +1,7 @@
 import re
-
-def fetch(l, col): return([int(max(list(map(int, re.findall("(\d+) " + col[i], l))))) for i in range(3)])
-
 d = open("i2.txt").read().split("\n")[:-1]; p = [0, 0]
 for i in range(len(d)):
-    z = fetch(d[i], ['r', 'g', 'b'])
+    z = (lambda l, col: [int(max(list(map(int, re.findall("(\d+) " + col[i], l))))) for i in range(3)])(d[i], ['r', 'g', 'b'])
     if all(x <= y for x, y in zip(z, [12, 13, 14])): p[0] += i + 1
     p[1] += (lambda r, g, b: r * g * b)(*z)
 p

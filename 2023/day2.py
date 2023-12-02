4bde04cef89466e6e3980c50b5a0e839e7a7d6f7
FarmPoet <49650908+FarmPoet@users.noreply.github.com>
2023-12-02 17:02:13 +0000

    Update day2.py

--- 2023/day2.py
+++ 2023/day2.py
@@ -1,20 +1,15 @@
 import re
 
-br = 12
-bg = 13
-bb = 14
-
+bag = [12, 13, 14]
 d = open("i2.txt").read().split("\n")[:-1]
-game = 0
-p1 = 0
-p2 = 0
+p = [0, 0, 0]
 
 for l in d:
-    game += 1
+    p[0] += 1
     r = int(max(list(map(int, re.findall("(\d+) r", l))), default = 0))
     g = int(max(list(map(int, re.findall("(\d+) g", l))), default = 0))
     b = int(max(list(map(int, re.findall("(\d+) b", l))), default = 0))
-    if r <= br and g <= bg and b <= bb: p1 += game
-    p2 += r * g * b
+    if r <= bag[0] and g <= bag[1] and b <= bag[2]: p[1] += p[0]
+    p[2] += r * g * b
 
-p1, p2
+p[1:]
